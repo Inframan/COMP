2fc23d3e3970cf4f5498dcebe40c25a3c4320ebd
Gabriel <gabrielsouto100d@gmail.com>
2015-05-15 01:33:45 +0100

    some functions

--- src/Listener - Zoid.java
+++ src/Listener - Zoid.java
@@ -1,27 +1,100 @@
+import java.lang.Object
+import java.util.regex.*
+
 public class Listener extends Airport_ParserBaseListener {
-    @Override
-    public void enterLattitude(Airport_ParserParser.LattitudeContext ctx) {
+    Pattern ident = Pattern.compile("[A-Za-Z]{0,5}");
+    Pattern floatnumber = Pattern.compile("[+-]?[0-9]+('.'[0-9]+)?");
+    Pattern heading = Pattern.compile("[0-360]('.'[0-9]+)?");
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterRed(@NotNull Airport_ParserParser.RedContext ctx)
+    {
         String str = ctx.getText();
-        Double val = Double.parseDouble(str.split("=")[1]);
-        if (val >= 90 || val <= -90)
-            System.out.println("Error: Latitude value out of bounds! Expected ]-90,90[ but got " + val);
+        int val = Integer.parseInt(str.split("=")[1]);
+        if(val < 0 || val > 255)
+            System.out.println("Error: Red value out of bounds! Expected [0,255] but got " + val);
+    }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+
+    @Override public void enterGreen(@NotNull Airport_ParserParser.GreenContext ctx)
+    {
+        String str = ctx.getText();
+        int val = Integer.parseInt(str.split("=")[1]);
+        if(val < 0 || val > 255)
+            System.out.println("Error: Blue value out of bounds! Expected [0,255] but got " + val);
+
+    }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+
+    @Override public void enterBlue(@NotNull Airport_ParserParser.BlueContext ctx)
+    {
+        String str = ctx.getText();
+        int val = Integer.parseInt(str.split("=")[1]);
+        if(val < 0 || val > 255)
+            System.out.println("Error: Green value out of bounds! Expected [0,255] but got " + val);
+
+
+    }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+
+    @Override public void enterApproach_runway(@NotNull Airport_ParserParser.Approach_runwayContext ctx)
+    {
+        String str = ctx.getText();
+        String value = str.split("=")[1];
+
+        if(value.equals("00"))
+            return;
+        if(value.startsWith("0")) {
+            int val = Integer.parseInt(value);
+            if (val < 0 || val > 9) {
+                System.out.println("Error: Aprroach_Runway value out of bounds! Expected [00,09] but got " + val);
+                return;
+            }
+        }
+        else
+        {
+            try
+            {
+                int d = Double.parseInt(value);
+                if (val < 0 || val > 36)
+                    System.out.println("Error: Aprroach_Runway value out of bounds! Expected [0,36] but got " + val);
+                return;
+            }
+            catch(NumberFormatException nfe)
+            {
+            }
+
+        }
+        if(!( value.equals("EAST") ||
+                value.equals("NORTH") ||
+                value.equals("NORTHEAST")  ||
+                value.equals("NORTHWEST")  ||
+                value.equals("SOUTH") ||
+                value.equals("SOUTHEAST")  ||
+                value.equals("SOUTHWEST")  ||
+                value.equals("WEST")  ||
+                value.equals("EAST") ))
+            System.out.println("Error: Aprroach_Runway value out of bounds! Expected cardinal points but got " + val);
+
     }
 
-    @Override
-    public void enterOrientation(Airport_ParserParser.OrientationContext ctx) {
-        String str = ctx.getText();
-        String val = str.split("=")[1];
-        if (val == "FORWARD" || val == "REVERSE")
-            System.out.println("Error: Orientation value not expected! Expected 'FORWARD' OR 'REVERSE' but got " + val);
-    }
-
-    @Override
-    public void enterTaxiwaypointType(Airport_ParserParser.TaxiwaypointTypeContext ctx) {
-        String str = ctx.getText();
-        String val = str.split("=")[1];
-        if (val == "NORMAL" || val == "HOLD_SHORT" || val == "ILS_HOLD_SHORT" || val == "HOLD_SHORT_NO_DRAWT" || val == "ILS_HOLD_SHORT_NO_DRAW")
-            System.out.println("Error: TaxiwaypointType value not expected! Expected 'NORMAL' or 'HOLD_SHORT' or  'ILS_HOLD_SHORT' or  'HOLD_SHORT_NO_DRAW' or 'ILS_HOLD_SHORT_NO_DRAW' but got " + val);
-    }
 
 
 }
